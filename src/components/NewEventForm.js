d92dddf825d562ba1708432f371414f2036d5ed1
NozimaOrinboyeva <orinboyevanozima275@gmail.com>
2025-10-18 11:27:49 +0500

    reset input delet edit

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -4,16 +4,26 @@ import { useState } from 'react'
 function NewEventForm() {
     const[title,setTitle] = useState('')
     const [date,setDate] = useState('')
+
+    const resetInputs = () => {
+        setTitle('')
+        setDate('')
+    }
     return (
         <form className="new-event-form">
             <label>
                 <span>Event Title:</span>
-                <input type="text" onChange={(e) => setTitle(e.target.value)}/>
+                <input type="text" onChange={(e) => setTitle(e.target.value)} value={title}/>
             </label>
+            <br />
             <label>
                 <span>Event Date:</span>
-                <input type="date" onChange={(e) => setDate(e.target.value)}/>
+                <input type="date" onChange={(e) => setDate(e.target.value)} value={date}/>
             </label>
+            <br />
+            <button onClick={resetInputs} type="button">reset inputs</button>
+            <br />
+            <br />
             <button>Submit</button>
             <hr/>
             <p>Titele: {title}</p>
